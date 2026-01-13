619e847ad776164d02f35a1b1bc2eac7477c82a9
rozer <shubhamsinha07084@gmail.com>
2026-01-13 23:48:12 +0530

    feat: registered blueprint in flask app

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
 from app.routes.url import url_bp
+from app.routes.text import text_bp
 
 app = Flask(__name__)
 CORS(app)
 
 # Register URL routes
 app.register_blueprint(url_bp)
+app.register_blueprint(text_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
