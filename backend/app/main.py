48bdefb7380c9a06314cfb1e56389d8d765809f6
rozer <shubhamsinha07084@gmail.com>
2026-01-14 15:08:28 +0530

    feat: modified flask app to resolve CORS issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,12 +3,17 @@ from flask_cors import CORS
 from app.routes.url import url_bp
 from app.routes.text import text_bp
 
-app = Flask(__name__)
-CORS(app)
+def create_app():
+    app = Flask(__name__)
+    CORS(app)  # Enable CORS for all routes
+    
+    # Register URL routes
+    app.register_blueprint(url_bp)
+    app.register_blueprint(text_bp)
+    
+    return app
 
-# Register URL routes
-app.register_blueprint(url_bp)
-app.register_blueprint(text_bp)
+app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
